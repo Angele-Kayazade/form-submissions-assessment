5630857a84681a5670ca23f0cd8ae6bd1bd5956b
Angele <anjelkayazade@gmail.com>
2023-01-08 20:08:47 -0500

    finished

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import "./index.css";
+
+function validateExists(value) {
+  return value && value.trim();
+}
+
+function performSearch(formData) {
+  const articleTitles = document.querySelectorAll("article h2")
+  const filteredArticles = Array.from(articleTitles).filter((articleTitle) => {
+    const title = articleTitle.innerHTML;
+    return title.toLowerCase().includes(formData.get("searchTerm").toLowerCase());
+});
+  
+  // Show only filtered articles
+  articleTitles.forEach((articleTitle) => {
+    articleTitle.parentNode.classList.add("hidden");
+  });
+  filteredArticles.forEach((articleTitle) => {
+    articleTitle.parentNode.classList.remove("hidden");
+  });
+}
+
+function validateForm(formData) {
+  // Check if seach term was entered
+  
+  
+  if (!validateExists(formData.get("searchTerm"))) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("error");
+    newDiv.id = "searchError";
+    const content = `Please enter a search term`;
+    newDiv.innerHTML = content;
+    
+    // Append to the form element
+  document.querySelector("#searchForm").appendChild(newDiv);
+  }
+  else {
+    performSearch(formData);
+  }
+}
+
+const searchHandler = (event) => {
+  event.preventDefault();
+  const form = event.target;
+  const formData = new FormData(form);
+ 
+  validateForm(formData)
+  
+  }
+
+
+const main = () => {
+  // Get the form element
+  const form = document.querySelector("#searchForm");
+
+  // Attach the search handler
+  form.addEventListener("submit", searchHandler);
+};
+
+
+window.addEventListener("DOMContentLoaded", main);
